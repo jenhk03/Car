9f52f2fd96a5e5a83f9e249c35c53dabe9953794
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-04-19 18:47:29 +0700

    Update DetailActivity.java

--- app/src/main/java/com/example/car/DetailActivity.java
+++ app/src/main/java/com/example/car/DetailActivity.java
@@ -1,14 +1,29 @@
 package com.example.car;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity {
-
+public class DetailActivity extends AppCompatActivity
+{
+    private ImageView ivPhoto;
+    private TextView tvName, tvAbout;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ivPhoto = findViewById(R.id.iv_photo);
+        tvName = findViewById(R.id.tv_name);
+        tvAbout = findViewById(R.id.tv_about);
+        Intent acc = getIntent();
+        String name = acc.getStringExtra("varName");
+        String about = acc.getStringExtra("varAbout");
+        String photo = acc.getStringExtra("varPhoto");
+        getSupportActionBar().setTitle(name);
+        tvName.setText(name);
+        tvAbout.setText(about);
     }
 }
