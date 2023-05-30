e1e58513193cb057941076a751483cf5930860d3
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-05-30 12:08:02 +0700

    Create CarModel.java

---
+++ app/src/main/java/com/example/car/Model/CarModel.java
@@ -0,0 +1,30 @@
+package com.example.car.Model;
+
+public class CarModel
+{
+    private String id, name, country, est, founder, description;
+    public String getId()
+    {
+        return id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getCountry()
+    {
+        return country;
+    }
+    public String getEst()
+    {
+        return est;
+    }
+    public String getFounder()
+    {
+        return founder;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+}
